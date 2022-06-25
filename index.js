489a4635e7d9fe2bade90f31e7cc52f5878d3517
Sheekar Banerjee <32336082+ac005sheekar@users.noreply.github.com>
2022-06-26 01:10:48 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {PythonShell} = require("python-shell");
 
 let option = {
     scriptPath: "C:/Users/USER/Documents/GitHub/Python-Node-JS-server",
-    args: ["James", "Bond" , 45],
+    args: ["James", "Bond" , 38],
 };
 PythonShell.run("C:/Users/USER/Documents/GitHub/Python-Node-JS-server/check.py", option, (err, res) => {
     if (err) console.log(err);
